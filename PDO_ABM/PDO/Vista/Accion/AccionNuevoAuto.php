d00c7d17f081006983db42235503e98fef67d9af
Nadia <nadiacarrasco83.nc@gmail.com>
2024-09-12 17:13:34 -0300

    se agrega archivo AccionNuevoAuto

--- PDO_ABM/PDO/Vista/Accion/AccionNuevoAuto.php
+++ PDO_ABM/PDO/Vista/Accion/AccionNuevoAuto.php
@@ -9,24 +9,29 @@ $autoExiste= $objAbmAuto->buscar(['Patente'=>$datosForm['Patente']]);
 
 $objAbmPersona=new ABM_Persona();
 $personaExiste= $objAbmPersona->buscar(['NroDni'=>$datosForm['DniDuenio']]);
-//print_r($datosForm['DniDuenio']);
-
-print_r($personaExiste);
-
-if(count($autoExiste)==0 && count($personaExiste)>0){
 
 
-    $respuesta= $objAbmAuto->alta($datosForm);
-    if($respuesta){
-        $respuesta="El auto fue ingresado a la base de datos correctamente";
+if($personaExiste!=null){ 
+    if(count($autoExiste)==0){
+
+
+        $respuesta= $objAbmAuto->alta($datosForm);
+        if($respuesta){
+            $respuesta="El auto fue ingresado a la base de datos correctamente";
+        }else{
+            $respuesta="El auto no pudo ser ingresado a la base de datos";
+        }
     }else{
-        $respuesta="El auto no pudo ser ingresado a la base de datos";
+        $respuesta="El auto ya se encuentra en la base de datos";
+        
     }
 }else{
-    $respuesta="El auto ya se encuentra en la base de datos";
+    $respuesta="Debe registrarse antes de ingresar un nuevo auto <br>"."<a href='../NuevaPersona.php'>Registrarse</a><br>";
+    
 }
 
 ?>
 <div>
-    <?php echo $respuesta; ?>
+    <?php echo $respuesta; ?><br>
+    <a href="../NuevoAuto.php">Volver</a>
 </div>

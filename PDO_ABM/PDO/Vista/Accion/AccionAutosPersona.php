10b8c6ee70f3a822e6430aa8ebe8164e55025169
Lucia Scalia <luciaxscaliax@gmail.com>
2024-09-11 22:01:03 -0300

    AccionAutosPersona

--- PDO_ABM/PDO/Vista/Accion/AccionAutosPersona.php
+++ PDO_ABM/PDO/Vista/Accion/AccionAutosPersona.php
@@ -13,48 +13,50 @@ $autosDuenio = $objAbmAuto->buscar(['DniDuenio'=>$datosForm['NroDni']]);
 $tieneAutos = count($autosDuenio) > 0 ? true : false;
 ?>
 
-<table border="1">
-    <th colspan="6">Persona</th>
-    <tr>
-        <th>DNI</th>
-        <th>Apellido</th>
-        <th>Nombre</th>
-        <th>Fecha de nacimiento</th>
-        <th>Tel&eacute;fono</th>
-        <th>Domicilio</th>
-    </tr>
-    <?php
-    echo "<tr>";
-    echo    "<td>".$duenio->getNroDni()."</td>";
-    echo    "<td>".$duenio->getApellido()."</td>";
-    echo    "<td>".$duenio->getNombre()."</td>";
-    echo    "<td>".$duenio->getFechaNac()."</td>";
-    echo    "<td>".$duenio->getTelefono()."</td>";
-    echo    "<td>".$duenio->getDomicilio()."</td>";
-    echo "</tr>";
+<?php
+if ($tieneAutos) {
+    echo "<table border='1'>
+            <th colspan='6'>Persona</th>
+            <tr>
+                <th>DNI</th>
+                <th>Apellido</th>
+                <th>Nombre</th>
+                <th>Fecha de nacimiento</th>
+                <th>Tel&eacute;fono</th>
+                <th>Domicilio</th>
+            </tr>";
+    echo    "<tr>";
+    echo       "<td>".$duenio->getNroDni()."</td>";
+    echo       "<td>".$duenio->getApellido()."</td>";
+    echo       "<td>".$duenio->getNombre()."</td>";
+    echo       "<td>".$duenio->getFechaNac()."</td>";
+    echo       "<td>".$duenio->getTelefono()."</td>";
+    echo       "<td>".$duenio->getDomicilio()."</td>";
+    echo    "</tr>";
     echo "</table>";
-    if ($tieneAutos) {
-        echo "<table border='1'>";
-        if (count($autosDuenio) == 1) {
-            echo "<th colspan='3'>Auto</th>";
-        } else {
-            echo "<th colspan='3'>Autos</th>";
-        }
-        
-        echo    "<tr>";
-        echo        "<th>Patente</th>";
-        echo        "<th>Marca</th>";
-        echo        "<th>Modelo</th>";
-        echo    "</tr>";
-        foreach ($autosDuenio as $unAuto) {
-            echo "<tr>";
-            echo    "<td>".$unAuto->getPatente()."</td>";
-            echo    "<td>".$unAuto->getMarca()."</td>";
-            echo    "<td>".$unAuto->getModelo()."</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
+
+    echo "<table border='1'>";
+
+    if (count($autosDuenio) == 1) {
+        echo "<th colspan='3'>Auto</th>";
     } else {
-        echo "No se encontraron autos a nombre de ".$duenio->getApellido()." ".$duenio->getNombre();
+        echo "<th colspan='3'>Autos</th>";
     }
-    ?>
+    
+    echo "<tr>
+            <th>Patente</th>
+            <th>Marca</th>
+            <th>Modelo</th>
+         </tr>";
+    foreach ($autosDuenio as $unAuto) {
+        echo "<tr>";
+        echo    "<td>".$unAuto->getPatente()."</td>";
+        echo    "<td>".$unAuto->getMarca()."</td>";
+        echo    "<td>".$unAuto->getModelo()."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    } else {
+        echo "No hay autos a nombre de ".$duenio->getApellido()." ".$duenio->getNombre();
+    }
+?>

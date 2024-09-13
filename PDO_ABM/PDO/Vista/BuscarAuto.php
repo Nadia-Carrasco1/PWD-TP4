76a46c6db37fe9b69f5b116beaea7039e0a56bce
Nadia <nadiacarrasco83.nc@gmail.com>
2024-09-13 18:41:44 -0300

    se agrega el header y carpeta bootstrap

--- PDO_ABM/PDO/Vista/BuscarAuto.php
+++ PDO_ABM/PDO/Vista/BuscarAuto.php
@@ -1,15 +1,13 @@
 <?php 
-$tituloPagina="Buscar Auto";
-//include_once()  aca poner direccion del encabezado
+$titulo="Buscar Auto";
+include_once ( 'Estructura/Header.php');
 
 ?>
-
-
-
-
-<form method="post" action="Accion/AccionBuscarAuto.php" enctype="multipart/form-data" id="formBuscarAuto">
-    <label>Ingrese su patente: </label><br>
-    <input id="Patente" class="Patente" name="Patente" type="text" required>
-    <input type="submit" value="Buscar">
-</form>
+<div class="">
+    <form method="post" action="Accion/AccionBuscarAuto.php" enctype="multipart/form-data" id="formBuscarAuto">
+        <label>Ingrese su patente: </label><br>
+        <input id="Patente" class="Patente" name="Patente" type="text" required>
+        <input type="submit" value="Buscar">
+    </form>
+</div>
 
---
+++ PDO_ABM/PDO/Vista/Estructura/Header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo?></title>
+    <link rel="stylesheet" href="../bootstrap-5.1.0-dist/css/bootstrap.min.css">
+    <script src="../bootstrap-5.1.0-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    

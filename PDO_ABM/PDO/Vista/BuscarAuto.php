3d4e3b6b6c0858e20aa4ef2e487727521d299bb7
Nadia <nadiacarrasco83.nc@gmail.com>
2024-09-14 22:48:49 -0300

    estilo a buscarauto

--- PDO_ABM/PDO/Vista/BuscarAuto.php
+++ PDO_ABM/PDO/Vista/BuscarAuto.php
@@ -3,15 +3,22 @@ $titulo="Buscar Auto";
 include_once ( 'Estructura/Header.php');
 
 ?>
-<div>
-<div>
-    <form method="post" action="Accion/AccionBuscarAuto.php" enctype="multipart/form-data" id="formBuscarAuto">
-        <label for ="Patente">Ingrese su patente: </label><br>
-        <input id="Patente" class="Patente" name="Patente" type="text" required>
-        <input type="submit" value="Buscar">
-    </form>
+<div class="container d-flex justify-content-center">
+    <div class="col-md-6">
+        <div class="d-flex justify-content-center">
+            <h3>Buscar Auto</h3>
+        </div>
+        <div class="d-flex justify-content-center">
+            <form method="post" action="Accion/AccionBuscarAuto.php" enctype="multipart/form-data" id="formBuscarAuto" class="needs-validation" novalidate> 
+                <label for ="Patente" class="form-label">Ingrese su patente: </label><br>
+                <input id="Patente" class="Patente" name="Patente" type="text" class="form-control" required>
+                <div class="d-flex justify-content-center">
+                    <input type="submit" value="Buscar" class="btn btn-success col-md-3">
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-<div>
 
 <?php $src= "Estructura/JS/buscarAuto.js";
 include_once 'Estructura/footer.php';

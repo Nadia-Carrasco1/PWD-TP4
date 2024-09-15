3812e39f080b209c5a6de2ff49bb5e2e9179e56a
Nadia <nadiacarrasco83.nc@gmail.com>
2024-09-14 23:11:18 -0300

    modificaciones buscarauto

--- PDO_ABM/PDO/Vista/BuscarAuto.php
+++ PDO_ABM/PDO/Vista/BuscarAuto.php
@@ -4,17 +4,17 @@ include_once ( 'Estructura/Header.php');
 
 ?>
 <div class="container d-flex justify-content-center">
-    <div class="col-md-6">
+    <div class="col-md-6"><br>
         <div class="d-flex justify-content-center">
             <h3>Buscar Auto</h3>
         </div>
         <div class="d-flex justify-content-center">
             <form method="post" action="Accion/AccionBuscarAuto.php" enctype="multipart/form-data" id="formBuscarAuto" class="needs-validation" novalidate> 
-                <label for ="Patente" class="form-label">Ingrese su patente: </label><br>
-                <input id="Patente" class="Patente" name="Patente" type="text" class="form-control" required>
+                <label for ="Patente" class="form-label text-muted">Ingrese su patente: </label><br>
+                <input id="Patente" class="Patente" name="Patente"  placeholder="DKR 675" type="text" class="form-control" required><br><br>
                 <div class="d-flex justify-content-center">
                     <input type="submit" value="Buscar" class="btn btn-success col-md-3">
-                </div>
+                </div><br>
             </form>
         </div>
     </div>

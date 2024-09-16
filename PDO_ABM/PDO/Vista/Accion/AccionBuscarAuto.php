0ab74acc105a1d120a0712815ef364d0d6457c74
Nadia <nadiacarrasco83.nc@gmail.com>
2024-09-16 11:35:11 -0300

    bootstrap en buscarauto y nuevoauto

--- PDO_ABM/PDO/Vista/Accion/AccionBuscarAuto.php
+++ PDO_ABM/PDO/Vista/Accion/AccionBuscarAuto.php
@@ -8,19 +8,20 @@ $objAbmAuto = new ABM_Auto();
 $datosAuto = $objAbmAuto->buscar(['Patente' => $patente]);
 $link = ".";
 include_once "../Estructura/Header.php";
+$icono = "<i class='bi bi-exclamation-triangle-fill'></i>";
+$alert = "danger";
 if (!empty($datosAuto)) {
     $auto = $datosAuto[0];
     
     if (isset($datosForm['Marca']) && isset($datosForm['Modelo'])) {
         // Verificar si la marca y el modelo coinciden
         if ($auto->getMarca() == $datosForm['Marca'] && $auto->getModelo() == $datosForm['Modelo']) {
-            echo "
-                <div class='container d-flex justify-content-center'>
-                    <div  class='col-md-6'><br>
-                        <div class='d-flex justify-content-center'>
-                            <h3>Datos del Auto</h3>
-                        </div>
-                        <div class='d-flex justify-content-center'>
+            $respuesta= "
+                            <div class='d-flex justify-content-center'>
+                                <h3>Datos del Auto</h3>
+                            </div>
+                     
+                            <div class='d-flex justify-content-center'>
                                 <table border='1' class='table table-sm text-center table-bordered'>
                                     <tr >
                                         <th>Patente</th>
@@ -33,44 +34,56 @@ if (!empty($datosAuto)) {
                                         <td>{$auto->getModelo()}</td>
                                         
                                     </tr>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                  
-                  
-                  ";
+                                </table>
+                            </div>";
+                            $icono=null;
+                            $alert=null;
         } else {
-            echo "La marca o el modelo no coinciden con los datos del auto.";
+            $respuesta= "La marca o el modelo no coinciden con los datos del auto.";
         }
     } else {
         // Mostrar formulario para ingresar marca y modelo
-        echo "
-        <div class='container d-flex justify-content-center'>
-            <div  class='col-md-6'>
-                <div class='d-flex justify-content-center'>
+        $respuesta= "
+     
                     <h4>Ingrese la Marca y el Modelo del Auto</h4>
-                </div>
+              
                 
                 <div class='d-flex justify-content-center'>
                     <form method='POST' action='' id='formAccionBuscarAuto'  class='needs-validation' novalidate>
                         <input type='hidden' class='form-control' name='Patente' value='{$patente}'>
-                        <label for='Marca' class='form-label text-muted'>Marca:</label>
-                        <input type='text' class='form-control' id='Marca' name='Marca' required>
-                        <label for='Modelo' class='form-label text-muted'>Modelo:</label>
+                        <label for='Marca' id='Marca' class='form-label text-muted'>Marca:</label>
+                        <input type='text' class='form-control' id='Marca' name='Marca' required><br>
+                        <label for='Modelo' id='Modelo' class='form-label text-muted'>Modelo:</label>
                         <input type='number' class='form-control' id='Modelo' name='Modelo' required><br>
                        <div class='d-flex justify-content-center'>
                             <input type='submit' value='Buscar' class='btn btn-success col-md-5'>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>";
+                </div>";
+           
+        $icono=null;
+        $alert=null;
+
     }
 } else {
-    echo "No se encontró ningún auto con la patente ingresada.";
+    $respuesta= "No se encontró ningún auto con la patente ingresada.";
 }
+$src= "../Estructura/JS/buscarAuto.js";
 
+echo "<br>
+<div class='container col-md-6'>
+    <div class='d-flex justify-content-center'>
+        <div class='alert alert-$alert'>
+            <div>
+            $icono
+            $respuesta
+            </div>
+        </div>
+    </div>
+</div>
+<div class='d-flex justify-content-center''>
+    <a href='../BuscarAuto.php' class='btn btn-success col-md-1'>Volver</a>
+</div><br>";
 
 include_once '../Estructura/footer.php';
 
--- PDO_ABM/PDO/Vista/Accion/AccionNuevoAuto.php
+++ PDO_ABM/PDO/Vista/Accion/AccionNuevoAuto.php
@@ -9,7 +9,8 @@ $autoExiste= $objAbmAuto->buscar(['Patente'=>$datosForm['Patente']]);
 
 $objAbmPersona=new ABM_Persona();
 $personaExiste= $objAbmPersona->buscar(['NroDni'=>$datosForm['DniDuenio']]);
-
+$icono = "<i class='bi bi-exclamation-triangle-fill'></i>";
+$alert = "danger";
 
 if($personaExiste!=null){ 
     if(count($autoExiste)==0){
@@ -18,6 +19,8 @@ if($personaExiste!=null){
         $respuesta= $objAbmAuto->alta($datosForm);
         if($respuesta){
             $respuesta="El auto fue ingresado a la base de datos correctamente";
+            $icono = "<i class='bi bi-check-circle-fill'></i>";
+            $alert = "success";
         }else{
             $respuesta="El auto no pudo ser ingresado a la base de datos";
         }
@@ -26,21 +29,25 @@ if($personaExiste!=null){
         
     }
 }else{
-    $respuesta="Debe registrarse antes de ingresar un nuevo auto <br>"."<a href='../NuevaPersona.php'>Registrarse</a><br>";
+    $respuesta="Debe registrarse antes de ingresar un nuevo auto"."<div class='d-flex justify-content-center'><a style='text-decoration: none; color: rgb(171,44,56); padding-left:2%; padding-right:2%; border:1px solid; border-radius:10px; background-color:#fff2f2;' href='../NuevaPersona.php'>Registrarse</a></div>";
     
 }
 $link = ".";
 include_once "../Estructura/Header.php";
 
-echo "<div class='container d-flex justify-content-center'>
-
-
-    {$respuesta}
-    <a href='../NuevoAuto.php'>Volver</a>
-   
-    
-    
-</div> ";
+echo " <br> <div class='container d-flex justify-content-center'>
+            <div class='d-flex justify-content-center'>
+                <div class='alert alert-$alert'>
+                    <div>
+                        $icono
+                        $respuesta
+                    </div>
+                </div> 
+            </div>
+        </div> 
+        <div class='d-flex justify-content-center''>
+        <a href='../NuevoAuto.php' class='btn btn-success col-md-1'>Volver</a>
+        </div><br>";
 
 include_once '../Estructura/footer.php';
 ?>

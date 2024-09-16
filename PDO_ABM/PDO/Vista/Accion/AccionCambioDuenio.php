32e95582ed465f5bdf447e41322f6548063b196e
Lucia Scalia <luciaxscaliax@gmail.com>
2024-09-15 22:30:38 -0300

    boostrap AccionCambioDuenio.php

--- PDO_ABM/PDO/Vista/Accion/AccionCambioDuenio.php
+++ PDO_ABM/PDO/Vista/Accion/AccionCambioDuenio.php
@@ -15,25 +15,45 @@ include_once "../Estructura/Header.php";
 ?>
 <div>
     <?php
+    $icono = "<i class='bi bi-exclamation-triangle-fill'></i>";
+    $alert = "danger";
     switch ($respuesta) {
         case -1:
-            echo "El cambio de dueño no pudo ser realizado";
+            $respuesta = "El cambio de dueño no pudo ser realizado";
             break;
         case 0:
-            echo "El cambio de dueño se realizó con éxito";
+            $respuesta = "El cambio de dueño se realizó con éxito";
+            $icono = "<i class='bi bi-check-circle-fill'></i>";
+            $alert = "success";
             break;
         case 1:
-            echo "La persona ingresada ya es propietaria del auto";
+            $respuesta = "La persona ingresada ya es propietaria del auto";
+            $alert = "warning";
             break;
         case 2; 
-            echo "La persona ni el auto se encuentran en la base de datos";
+            $respuesta = "La persona ni el auto se encuentran en la base de datos";
             break;
         case 3:
-            echo "La persona ingresada no se encuentra en la base de datos";
+            $respuesta = "La persona ingresada no se encuentra en la base de datos";
             break;
         case 4:
-            echo "El auto ingresado no se encuentra en la base de datos";
+            $respuesta = "El auto ingresado no se encuentra en la base de datos";
     }
+    echo "<br>
+    <div class='container col-md-6'>
+    <div class='d-flex justify-content-center'>
+        <div class='alert alert-$alert'>
+            <div>
+            $icono
+            $respuesta
+            </div>
+        </div>
+    </div>
+        <div class='d-flex justify-content-center' action=''>
+            <a href='../CambioDuenio.php' class='btn btn-success col-md-2'>Volver</a>
+        </div>
+    </div>
+    ";
     ?>
 </div>
 <?php
--- PDO_ABM/PDO/Vista/Estructura/Header.php
+++ PDO_ABM/PDO/Vista/Estructura/Header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title><?php echo $titulo?></title>    
 </head>
 <body>

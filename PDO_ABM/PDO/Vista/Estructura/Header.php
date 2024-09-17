5d902e32ff6c9390e39b0808cb01fd5c7bac1144
Lucia Scalia <luciaxscaliax@gmail.com>
2024-09-16 21:29:26 -0300

    acomodado footer al final de la pagina

--- PDO_ABM/PDO/Vista/Estructura/Header.php
+++ PDO_ABM/PDO/Vista/Estructura/Header.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/style.css">
+    <link rel="stylesheet" href="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/styleFooter.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title><?php echo $titulo?></title>    
 </head>
 <body>
+<main>
 <?php
 include_once 'Menu.php';
 ?>
--- PDO_ABM/PDO/Vista/Estructura/footer.php
+++ PDO_ABM/PDO/Vista/Estructura/footer.php
@@ -1,13 +1,28 @@
-<div style="background-color:#212529;color:white;" class="dentro" id="dentro">
-    <footer class="pie">
+<div class="push"></div>
+</main>
+<div style="background-color:#212529;color:white;" class="footer" id="footer">
+    <footer>
         <script src="<?php echo $link?>./Estructura/JS/jquery-3.7.1.min.js"></script>
         <script src="<?php echo $link?>./Estructura/JS/jquery-validation-1.19.5/dist/jquery.validate.min.js"></script>
         <script src="<?php echo $link?>./Estructura/CSS/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
         <script src="<?php echo $src?>"></script>
 
-        <div><h5>PWD 2024 - GRUPO 23</h5></div>
-        <div><h6>Integrantes:</h6></div>
-        <div><p>Scalia Lucia y Carrasco Nadia<p></div>
+        <div class="d-flex justify-content-center">
+            <div class="footerInfo"">
+                <h6>Programaci&oacute;n Web Din&aacute;mica - GRUPO 23</h6>
+                <ul style="list-style-type: none;padding: 0px; margin: 0px;">
+                    <li>Tecnicatura Universitaria en Desarrollo Web</li>
+                    <li>PWD, 2° cuatrimestre 2024</li>
+                </ul>
+            </div>
+            <div class="footerInfo">
+                <h6>Integrantes:</h6>
+                <ul>
+                    <li>Scalia Lucia FAI-4235</li>
+                    <li>Carrasco Nadia FAI-4236</li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </div>
 </body>
